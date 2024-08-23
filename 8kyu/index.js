a991a704c12c0904a891a87088c870cbe049f298
Udonna Okebata <udonnaokebata@gmail.com>
2024-08-23 21:43:31 +0100

    Cocktail drink

--- 8kyu/index.js
+++ 8kyu/index.js
@@ -1,1 +1,20 @@
-// Hello everyone
+
+
+
+
+document.getElementById("search").addEventListener("click", getDrink);
+
+function getDrink() {
+  let input = document.querySelector("input").value;
+
+  fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${input}`)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data.drinks[0]);
+
+      document.querySelector("h1").innerText = data.drinks[0].strDrink;
+      document.querySelector("img").src = data.drinks[0].strDrinkThumb;
+      document.getElementById("name").innerText = data.drinks[0].strAlcoholic;
+      document.querySelector("p").innerText = data.drinks[0].strInstructions;
+    });
+}
